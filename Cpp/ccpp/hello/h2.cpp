993bc693d170808eed9b6c9eedc229e96ea90643
Shubham Kale <78206080+Skale3628@users.noreply.github.com>
2026-02-04 15:02:11 +0530

    Add files via upload

---
+++ Cpp/ccpp/hello/h2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+void fun(){
+    std::cout<<"in fun"<<std::endl;
+}
+    /*::main' must return 'int'
+    void main(){
+     */         
+int main(){
+    fun();
+}
---
+++ Cpp/ccpp/hello/h3.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+//using namespace std;
+int main(){
+    int cout=10;
+    std::cout<<"cout"<<std::endl;
+    return 0;
+}
